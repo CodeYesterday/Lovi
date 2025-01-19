bc81632b03d0fe6a4fa41771362819cdd61e8430
Reinhard <osre@aarpg.net>
2025-01-19 17:51:08 +0100

    Added info popup button

---
+++ source/CodeYesterday.Lovi/Components/InfoButton.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+using Radzen.Blazor.Rendering;
+using System.Text;
+
+namespace CodeYesterday.Lovi.Components;
+
+public partial class InfoButton
+{
+    private RadzenButton _button = default!;
+    private Popup _popup = default!;
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string? Width { get; set; }
+
+    [Parameter]
+    public string? Height { get; set; }
+
+    private string GetPopupStyle()
+    {
+        var sb = new StringBuilder();
+
+        if (Width is not null)
+        {
+            sb.Append($"width: {Width};");
+        }
+        if (Height is not null)
+        {
+            sb.Append($"height: {Height};");
+        }
+
+        return sb.ToString();
+    }
+}
