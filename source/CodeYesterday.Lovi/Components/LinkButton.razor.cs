f86fcec8c0e2269dcd4c7c7d8da494cd5b1a266b
Reinhard <osre@aarpg.net>
2025-01-19 17:51:20 +0100

    Added Link button

---
+++ source/CodeYesterday.Lovi/Components/LinkButton.razor.cs
@@ -0,0 +1,33 @@
+﻿using CodeYesterday.Lovi.Services;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace CodeYesterday.Lovi.Components;
+
+public partial class LinkButton
+{
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
+
+    [Parameter]
+    public string Uri { get; set; } = string.Empty;
+
+    [Parameter]
+    public string? Text { get; set; }
+
+    [Inject]
+    private TooltipService TooltipService { get; set; } = default!;
+
+    [Inject]
+    private ISettingsService SettingsService { get; set; } = default!;
+
+    private Task OpenUri()
+    {
+        return Browser.Default.OpenAsync(Uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private void OpenTooltip(ElementReference e)
+    {
+        TooltipService.Open(e, Uri, SettingsService.Settings.TooltipOptions);
+    }
+}
